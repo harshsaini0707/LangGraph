1fd7f1e5d92f66f658b5e143d5339c43be30f2b3
harshsaini0707 <harshsaini0172@gmail.com>
2025-10-03 22:05:29 +0530

    Adding new node and create new edge

--- LangGraph/index.ts
+++ LangGraph/index.ts
@@ -6,7 +6,9 @@ import {Annotation, StateGraph , START , END} from "@langchain/langgraph"
 const State = Annotation.Root({
   height: Annotation<string>(),
   weight: Annotation<string>(),
+  category :  Annotation<string>(),
   result: Annotation<number>(),
+
 });
 // Without Annotation, StateGraph doesn’t know what type of state you want to flow through the graph.
 
@@ -25,7 +27,21 @@ graph.addNode("calculateBMI", async (state: typeof State.State) => {
   return state;
 });
 
+//Node 2:
+graph.addNode('label_bmi' ,  async(state : typeof State.State)=>{
+  const res =  state.result;
 
+  if(res < 18.5){
+    state.category ='Underweight'
+  }else if(18.5 <= res && res <25){
+    state.category = 'Normal'
+  }else if(25 > res && res < 30){
+    state.category = "Overweight"
+  }else{
+    state.category = "Obese"
+  }
+  return state
+})  
 
 /**
  * START , END they were just like a dummy node that tell that from where graph starts and ends
@@ -33,7 +49,8 @@ graph.addNode("calculateBMI", async (state: typeof State.State) => {
 
 // add edges
 graph.addEdge(START , 'calculateBMI');
-graph.addEdge('calculateBMI' , END)
+graph.addEdge('calculateBMI' , 'label_bmi');
+graph.addEdge('label_bmi' , END)
 
 
 //compile the graph
@@ -45,7 +62,7 @@ const app =  graph.compile(); //return a compiles graph object
 
 (async () => {
   const result = await app.invoke({ height: "1.80", weight: "75" });
-  console.log(Math.round(result.result)); // return a final/output state
+  console.log(result); // return a final/output state
 
 })();
 
